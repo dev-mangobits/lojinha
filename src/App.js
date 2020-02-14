7f3e56cf331f2c140abe35bd054c274b284d53aa
dev-mangobits <dev@mangobits.com>
2020-02-14 01:41:24 -0300

    Teste

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img
+          src="http://admin.startupkit.mangotest.com/img/mango.png"
+          className="App-logo"
+          alt="logo"
+        />
+        <br />
+        <p>Nome do projeto</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://www.mangobits.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lojinha
         </a>
       </header>
     </div>
